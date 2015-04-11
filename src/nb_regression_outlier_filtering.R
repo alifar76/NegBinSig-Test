04cf2dc1d1530d6f11b25ef51f964633142a8e0b
Ali Ahmad Faruqi <alifar76@gmail.com>
2015-04-10 17:50:31 -0700

    added poisson, aic, bic

--- src/nb_regression_outlier_filtering.R
+++ src/nb_regression_outlier_filtering.R
@@ -1,4 +1,4 @@
-#Rscript nb_regression_outlier_filtering_v2.R high_vs_low_otu_table.txt high_low_mapfile.txt High Low Treatment ZINB_NB_Output_result.txt 2
+#Rscript nb_regression_outlier_filtering.R high_vs_low_otu_table.txt high_low_mapfile.txt High Low Treatment ZINB_NB_Output_result.txt 2
 
 start.time <- Sys.time()
 
@@ -30,39 +30,58 @@ zinb_nb_test <- function(both,MYdata,trt,categ1,categ2){
     final_vec <- c()
     formula1 <- as.formula(paste("both[,i] ~ ",trt," | 1",sep=""))
     formula2 <- as.formula(paste("both[,i] ~ ",trt,sep=""))
-    result.zinb <- tryCatch(zeroinfl(formula1, data = both, dist = "negbin"),error=function(e) NA)
-    result.nb <- tryCatch(glm.nb(formula2, data = both),error=function(e) NA)
-    zinb.coeff <- tryCatch(exp(summary(result.zinb)$coefficients$count[2,1]),error=function(e) NA)
-    nb.coeff <- tryCatch(exp(summary(result.nb)$coefficients[2,1]),error=function(e) NA)
-    zinb.pval <- tryCatch(summary(result.zinb)$coefficients$count[2,4],error=function(e) NA)
-    nb.pval <- tryCatch(summary(result.nb)$coefficients[2,4],error=function(e) NA)
-    final_vec <- c(zinb.coeff,zinb.pval,nb.coeff,nb.pval)
-    shap_wilk_pval <- tryCatch(shapiro.test(both[,i])$p.value,error=function(e) NA)       # Significant p-value indicates data is not normally distributed.
-    pval_ttest <- tryCatch(t.test(formula2, data=both)$p.value,error=function(e) NA)
-    estimate_tab <- tryCatch(t.test(formula2, data=both)$estimate,error=function(e) NA)
-    heading <- paste(gsub(" ","",strsplit(names(estimate_tab)[1],"mean in group")[[1]][2],fixed=TRUE),"_minus_",gsub(" ","",strsplit(names(estimate_tab)[2],"mean in group")[[1]][2],fixed=TRUE),"_mean",sep="")
-    mean_diff <- tryCatch((estimate_tab[1][[1]] - estimate_tab[2][[1]]),error=function(e) NA)
-    kwtest <- tryCatch(kruskal.test(formula2,data=both)$p.value,error=function(e) NA)
-    warn.nb <- tryCatch(glm.nb(formula2, data = both),error=function(e) NA,warning=function(w) w)
-    valwarn.nb <- ifelse(class(warn.nb)[1] == "simpleWarning", 'yes', 'no')
+    result.pois <- tryCatch(glm(formula2, family="poisson", data = both),error=function(e) NA)
+    result.zinb <- tryCatch(zeroinfl(formula1, data = both, dist = "negbin"),error=function(e) NA)			
+    result.nb <- tryCatch(glm.nb(formula2, data = both),error=function(e) NA)    
+    
+    
+    pois.coeff <- tryCatch(exp(summary(result.pois)$coefficients[2,1]),error=function(e) NA)			# Column 1
+    pois.pval <- tryCatch(summary(result.pois)$coefficients[2,4],error=function(e) NA)					# Column 2
+    nb.coeff <- tryCatch(exp(summary(result.nb)$coefficients[2,1]),error=function(e) NA)				# Column 3
+    nb.pval <- tryCatch(summary(result.nb)$coefficients[2,4],error=function(e) NA)						# Column 4
+    zinb.coeff <- tryCatch(exp(summary(result.zinb)$coefficients$count[2,1]),error=function(e) NA)		# Column 5
+    zinb.pval <- tryCatch(summary(result.zinb)$coefficients$count[2,4],error=function(e) NA)			# Column 6
+	aic.pois <- tryCatch(AIC(result.pois),error=function(e) NA)											# Column 7
+	aic.nb <- tryCatch(AIC(result.pois),error=function(e) NA)											# Column 8
+	aic.zinb <- tryCatch(AIC(result.pois),error=function(e) NA)											# Column 9
+	bic.pois <- tryCatch(BIC(result.pois),error=function(e) NA)											# Column 10
+	bic.nb <- tryCatch(BIC(result.pois),error=function(e) NA)											# Column 11
+	bic.zinb <- tryCatch(BIC(result.pois),error=function(e) NA)											# Column 12
+  final_vec <- c(pois.coeff,pois.pval,nb.coeff, nb.pval, zinb.coeff, zinb.pval, aic.pois, aic.nb, aic.zinb, bic.pois, bic.nb, bic.zinb)						# Appended data from Columns 1-12
+    
+    shap_wilk_pval <- tryCatch(shapiro.test(both[,i])$p.value,error=function(e) NA)       # Significant p-value indicates data is not normally distributed. (Column 15)
+    pval_ttest <- tryCatch(t.test(formula2, data=both)$p.value,error=function(e) NA)		# Column 14
+    estimate_tab <- tryCatch(t.test(formula2, data=both)$estimate,error=function(e) NA)		# Column 17-20 drawn from this object
+    heading <- paste(gsub(" ","",strsplit(names(estimate_tab)[1],"mean in group")[[1]][2],fixed=TRUE),"_minus_",gsub(" ","",strsplit(names(estimate_tab)[2],"mean in group")[[1]][2],fixed=TRUE),"_mean",sep="")		# Column 16
+    mean_diff <- tryCatch((estimate_tab[1][[1]] - estimate_tab[2][[1]]),error=function(e) NA)			# Column 13
+    kwtest <- tryCatch(kruskal.test(formula2,data=both)$p.value,error=function(e) NA)					# Column 21
+    warn.nb <- tryCatch(glm.nb(formula2, data = both),error=function(e) NA,warning=function(w) w)		
+    valwarn.nb <- ifelse(class(warn.nb)[1] == "simpleWarning", 'yes', 'no')								# Column 22
     trt1vals <- both[,i][which(as.vector(both[,trt]) == categ1)]
     trt2vals <- both[,i][which(as.vector(both[,trt]) == categ2)]
-    zerotrt1 <- sum(trt1vals == 0)
-    zerotrt2 <- sum(trt2vals == 0)
-    nonzerotrt1 <- length(trt1vals) - zerotrt1
-    nonzerotrt2 <- length(trt2vals) - zerotrt2
-    totaltrt1 <- sum(trt1vals)
-    totaltrt2 <- sum(trt2vals)
-    mean.otu <- mean(both[,i])
-    var.otu <- var(both[,i])
-    var.mean.ratio <- var.otu/mean.otu
-    
+    zerotrt1 <- sum(trt1vals == 0)																		# Column 23
+    zerotrt2 <- sum(trt2vals == 0)																		# Column 24
+    nonzerotrt1 <- length(trt1vals) - zerotrt1															# Column 25
+    nonzerotrt2 <- length(trt2vals) - zerotrt2															# Column 26
+    totaltrt1 <- sum(trt1vals)																			# Column 27
+    totaltrt2 <- sum(trt2vals)																			# Column 28
+    mean.otu <- mean(both[,i])																			# Column 29
+    var.otu <- var(both[,i])																			# Column 30
+    var.mean.ratio <- var.otu/mean.otu																	# Column 31
     newd <- both[,i][-(which(both[,i] > 5*IQR(both[,i])))]       # Select indices of values that are not greater than 5 times the IQR (i.e., values > 5*IQR will be removed)
-	 treatment <- as.vector(both[,trt])
-	 newmeta <- treatment[-(which(both[,i] > 5*IQR(both[,i])))]    # Select values greater than 5 times the IQR
-	 newpval_nb <- tryCatch(summary(glm.nb(newd ~ newmeta))$coefficients[2,4],error=function(e) NA)
+	treatment <- as.vector(both[,trt])
+	newmeta <- treatment[-(which(both[,i] > 5*IQR(both[,i])))]    # Select values greater than 5 times the IQR
+	newpval_pois <- tryCatch(summary(glm(newd ~ newmeta, family="poisson"))$coefficients[2,4],error=function(e) NA)		# Column 32
+	newpval_nb <- tryCatch(summary(glm.nb(newd ~ newmeta))$coefficients[2,4],error=function(e) NA)						# Column 33
+	newpval_zinb <-  tryCatch(summary(zeroinfl(newd ~ newmeta | 1, dist = "negbin"))$coefficients$count[2,4],error=function(e) NA)	# Column 34
+	aic.filt.pois <- tryCatch(AIC(glm(newd ~ newmeta, family="poisson")),error=function(e) NA)										# Column 35
+	aic.filt.nb <- tryCatch(AIC(glm.nb(newd ~ newmeta)),error=function(e) NA)														# Column 36
+	aic.filt.zinb <- tryCatch(AIC(zeroinfl(newd ~ newmeta | 1, dist = "negbin")),error=function(e) NA)								# Column 37
+	bic.filt.pois <- tryCatch(BIC(glm(newd ~ newmeta, family="poisson")),error=function(e) NA)										# Column 38
+	bic.filt.nb <- tryCatch(BIC(glm.nb(newd ~ newmeta)),error=function(e) NA)														# Column 39
+	bic.filt.zinb <- tryCatch(BIC(zeroinfl(newd ~ newmeta | 1, dist = "negbin")),error=function(e) NA)								# Column 40
 
-    final_vec <- c(final_vec,mean_diff,pval_ttest,shap_wilk_pval,heading,estimate_tab[1][[1]],names(estimate_tab)[1],estimate_tab[2][[1]],names(estimate_tab)[2],kwtest,valwarn.nb,zerotrt1,zerotrt2,nonzerotrt1,nonzerotrt2,totaltrt1,totaltrt2,mean.otu,var.otu,var.mean.ratio,newpval_nb)
+    final_vec <- c(final_vec,mean_diff,pval_ttest,shap_wilk_pval,heading,estimate_tab[1][[1]],names(estimate_tab)[1],estimate_tab[2][[1]],names(estimate_tab)[2],kwtest,valwarn.nb,zerotrt1,zerotrt2,nonzerotrt1,nonzerotrt2,totaltrt1,totaltrt2,mean.otu,var.otu,var.mean.ratio,newpval_pois,newpval_nb,newpval_zinb,aic.filt.pois,aic.filt.nb,aic.filt.zinb,bic.filt.pois,bic.filt.nb,bic.filt.zinb)			# 
     final_vec
   }
   return (all_data)
@@ -74,25 +93,28 @@ final_steps <- function(otutable,mapfile,categ1,categ2,trt,outputname){
   both <- data_load(MYdata,mapfile,trt,categ1,categ2)
   all_data <- zinb_nb_test(both,MYdata,trt,categ1,categ2)
   allcols <- length(colnames(MYdata))
-  zinb.qval <- p.adjust(all_data[,2], method = "fdr")
-  nb.qval <-  p.adjust(all_data[,4], method = "fdr")
-  ttest.qval <- p.adjust(all_data[,6], method = "fdr")
-  kw.qval <- p.adjust(all_data[,13], method = "fdr")
-  outlier_filt_qval <- p.adjust(all_data[,24], method = "fdr")
-  taxonomy <- MYdata[allcols]
-  otuids <- colnames(both)[2:(length(colnames(both))-1)]
-  taxlabels <- as.vector(taxonomy[,1])
-  difflabel <- unique(all_data[,8])
-  mean1_head <- unique(all_data[,10])
-  mean2_head <- unique(all_data[,12])
- zrtrt1 <- paste("# of 0's in ",categ1,sep="")
- zrtrt2 <- paste("# of 0's in ",categ2,sep="")
+  pois.qval <- p.adjust(all_data[,2], method = "fdr")					# Column 2 of all_data 
+  nb.qval <-  p.adjust(all_data[,4], method = "fdr")					# Column 4 of all_data
+  zinb.qval <- p.adjust(all_data[,6], method = "fdr")					# Column 6 of all_data
+  ttest.qval <- p.adjust(all_data[,14], method = "fdr")					# Column 14 of all_data
+  kw.qval <- p.adjust(all_data[,21], method = "fdr")					# Column 21 of all_data
+  pois.filt.qval <- p.adjust(all_data[,32], method = "fdr")				# Column 32 from all_data
+  nb.filt.qval <- p.adjust(all_data[,33], method = "fdr")				# Column 33 from all_data
+  zinb.filt.qval <- p.adjust(all_data[,34], method = "fdr")				# Column 34 from all_data
+  	taxonomy <- MYdata[allcols]
+ 	otuids <- colnames(both)[2:(length(colnames(both))-1)]
+	taxlabels <- as.vector(taxonomy[,1])
+	difflabel <- unique(all_data[,16])						# It's the unique entry of 'heading' column in all_data
+	mean1_head <- unique(all_data[,18])					# It's the unique 'names(estimate_tab)[1]' column in all_data
+	mean2_head <- unique(all_data[,20])					# It's the unique entry of 'names(estimate_tab)[2]' column in all_data
+ 	zrtrt1 <- paste("# of 0's in ",categ1,sep="")
+ 	zrtrt2 <- paste("# of 0's in ",categ2,sep="")
  nzrtrt1 <- paste("# of non-zeroes in ",categ1,sep="")
  nzrtrt2 <- paste("# of non-zeroes in ",categ2,sep="")
  tottrt1 <- paste("Total count in ",categ1,sep="")
  totttrt2 <- paste("Total count in ",categ2,sep="")
-  all_data <- cbind(otuids,all_data[,1:2],zinb.qval,all_data[,3:4],nb.qval,all_data[,9],all_data[,11],all_data[,5:6],ttest.qval,all_data[,13],kw.qval,all_data[,14:23],all_data[,7],taxlabels,all_data[,24],outlier_filt_qval)
-  colnames(all_data) <- c("OTU_IDs","ZINB_Coeff","ZINB_pval","ZINB_qval","NB_Coeff","NB_pval","NB_qval",mean1_head,mean2_head,difflabel,"ttest_pval","ttest_qval","KW_pval","KW_qval","NB_Coeff_Estimate_Error",zrtrt1,zrtrt2,nzrtrt1,nzrtrt2,tottrt1,totttrt2,"mean_otu","variance_otu","var/mean ratio","Shapiro_Wilk_Normality_pvalue","taxonomy","outlier_nbpval","outlier_nbqval") #change Difference to groups being tested
+  all_data <- cbind(otuids,all_data[,1:2],pois.qval,all_data[,3:4],nb.qval,all_data[,5:6],zinb.qval,all_data[,17],all_data[,19],all_data[,13],all_data[,14],ttest.qval,all_data[,21],kw.qval,all_data[,22:31],all_data[,15],taxlabels,all_data[,32],pois.filt.qval,all_data[,33],nb.filt.qval,all_data[,34],zinb.filt.qval,all_data[,7:12],all_data[,35:40])
+  colnames(all_data) <- c("OTU_IDs","Poiss_Coeff","Poiss_pval","Poiss_qval","NB_Coeff","NB_pval","NB_qval","ZINB_Coeff","ZINB_pval","ZINB_qval",mean1_head,mean2_head,difflabel,"ttest_pval","ttest_qval","KW_pval","KW_qval","NB_Coeff_Estimate_Error",zrtrt1,zrtrt2,nzrtrt1,nzrtrt2,tottrt1,totttrt2,"mean_otu","variance_otu","var/mean ratio","Shapiro_Wilk_Normality_pvalue","taxonomy","pois_filt_pval","pois_filt_qval","nb_filt_pval","nb_filt_qval","zinb_filt_pval","zinb_filt_qval","aic.pois", "aic.nb", "aic.zinb", "bic.pois", "bic.nb", "bic.zinb","aic.filt.pois","aic.filt.nb","aic.filt.zinb","bic.filt.pois","bic.filt.nb","bic.filt.zinb") #change Difference to groups being tested
   write.table(as.matrix(all_data),file=outputname,sep="\t",append = TRUE,col.names=TRUE,row.names=FALSE,quote=FALSE)
 }
 
